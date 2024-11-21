0036b8fba57b25b59ccb68fc3269ade500ce257d
mkpage1337 <mrku1337@gmail.com>
2024-11-21 22:11:23 +0100

    feature/login

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,32 @@ import Navigation from "./components/Navigation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import Settings from "./components/Settings";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
     <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<ExpenseTracker />} />
-        <Route path="/clients" element={<Clients />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/reports" element={<Reports />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-      <Footer />
+      <SignedIn>
+        <UserButton />
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<ExpenseTracker />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+        <Footer />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
     </Router>
   );
 }
