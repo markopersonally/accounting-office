7174bbf3e5432c46f5d6a7e8ff1e9e02bb0a46f1
mkpage1337 <mrku1337@gmail.com>
2024-11-26 20:16:00 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,24 @@
-import { SignedIn, UserButton } from "@clerk/clerk-react";
-
 export default function Home() {
-  return <div>HOME PAGE</div>;
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-24">
+      <div className="text-center">
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          Welcome to <span className="text-indigo-600">l i m i t i.</span>
+        </h1>
+        <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+          Simple modern dashboard to office.
+        </p>
+        <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+          <div className="rounded-md shadow">
+            <a
+              href="#"
+              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+            >
+              Get started
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
