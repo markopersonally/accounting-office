bccd87158f419807f51b6f2079f50d2117019e12
mkpage1337 <mrku1337@gmail.com>
2024-11-30 20:18:00 +0100

    refactor

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
-import Footer from "../components/Footer";
+import Navigation from "../components/Navigation.jsx";
+import Footer from "../components/Footer.jsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -17,6 +18,7 @@ export default function RootLayout() {
       routerReplace={(to) => navigate(to, { replace: true })}
       publishableKey={PUBLISHABLE_KEY}
     >
+      <Navigation />
       <main className="h-screen">
         <Outlet />
       </main>
